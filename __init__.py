d8d734eca94e731fcdaa44772477225dfa4edb54
BassXT <91958461+BassXT@users.noreply.github.com>
2024-12-09 15:43:27 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,6 +1,2 @@
-from homeassistant.core import HomeAssistant
-
-async def async_setup(hass: HomeAssistant, config: dict):
-    """Setup the xTool F1 integration."""
-    hass.states.async_set("xtool_f1.status", "unknown")
-    return True
+# __init__.py
+"""The xTool integration."""
